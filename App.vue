c6330a33aff48314cad742ad8994f22e262fbda7
qinhuaigan <624518812@qq.com>
2022-06-17 13:59:14 +0800

    更新版本

---
+++ App.vue
@@ -0,0 +1,18 @@
+<style lang="scss">
+	/* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
+	@import "uview-ui/index.scss";
+</style>
+<script>
+	export default {
+		onLaunch: function() {
+		},
+		onShow: function() {
+		},
+		onHide: function() {
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
